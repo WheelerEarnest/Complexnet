5051de586129c097848b5f12134ce4182c00ea80
JWheelerEarnest <jwheelerearnest@gmail.com>
2018-09-19 15:36:53 -0400

    Implemented a function to convert complex numpy data to its float representation and vice versa

--- util.py
+++ util.py
@@ -18,3 +18,23 @@ def c_elem_mult(x, y, units):
     real = (a * c) - (b * d)
     imag = (a * d) + (b * c)
     return K.concatenate([real, imag], axis = -1)
+
+def complex_to_float(a):
+    # Converts numpy data that is complex64 or complex128 and makes it the float representation
+    # so a complex vector would look like [real, imaginary]
+    assert a.dtype is np.dtype('complex64') or a.dtype is np.dtype('complex128')
+    return np.concatenate((a.real, a.imag), axis=0)
+
+def float_to_complex(a):
+    # Converts numpy data that is float32 or float64 to complex representation
+    assert a.dtype is np.dtype('float32') or a.dtype is np.dtype('float64')
+    shape = np.shape(a)
+    assert shape[0] % 2 == 0
+    mid = shape[0] // 2
+    if a.dtype is np.dtype('float32'):
+        c = np.empty((mid, shape[1]), dtype='complex64')
+    else:
+        c = np.empty((mid, shape[1]), dtype='complex128')
+    c.real = a[:mid, :]
+    c.imag = a[mid:, :]
+    return c
